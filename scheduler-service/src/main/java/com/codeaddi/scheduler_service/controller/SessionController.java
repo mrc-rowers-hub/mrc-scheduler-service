64886f399077c3b968aca8ae24deac1167b31052
AdelaideBaron <baronadelaide@hotmail.com>
2024-08-12 21:39:46 +0100

    update session functionality

--- scheduler-service/src/main/java/com/codeaddi/scheduler_service/controller/SessionController.java
+++ scheduler-service/src/main/java/com/codeaddi/scheduler_service/controller/SessionController.java
@@ -1,18 +1,19 @@
 package com.codeaddi.scheduler_service.controller;
 
 import com.codeaddi.scheduler_service.controller.db.SessionsClient;
+import com.codeaddi.scheduler_service.model.http.StandardResponse;
+import com.codeaddi.scheduler_service.model.http.enums.Status;
 import com.codeaddi.scheduler_service.model.repository.Session;
 import com.codeaddi.scheduler_service.model.repository.SessionRepository;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RequestMapping("/sessions")
+@RequestMapping("/standard_sessions")
 @RestController
 @Slf4j
 public class SessionController {
@@ -25,5 +26,21 @@ public class SessionController {
         log.info("Retrieving all sessions");
         return ResponseEntity.ok(sessionsClient.getAllSessions());
     }
+
+    @PutMapping("/update_session")
+    public ResponseEntity<StandardResponse> updateSession(@RequestBody Session newSession){
+        log.info("UPDATE REQUEST RECEIVED");
+
+        try{
+            sessionsClient.replaceSession(newSession);
+            return ResponseEntity.ok().body(StandardResponse.builder().status(Status.SUCCESS).message("Session replaced").build());
+        } catch(EntityNotFoundException e){
+            log.error("Session with id {} not found, creating new session", newSession.getId());
+            sessionsClient.addSession(newSession);
+            return ResponseEntity.badRequest().body(StandardResponse.builder().status(Status.SUCCESS_WITH_WARNING).message("Session not found, new session made").build());
+        }
+    }
+
+// delete session
 
 }
--- scheduler-service/src/main/java/com/codeaddi/scheduler_service/controller/db/SessionsClient.java
+++ scheduler-service/src/main/java/com/codeaddi/scheduler_service/controller/db/SessionsClient.java
@@ -2,11 +2,14 @@ package com.codeaddi.scheduler_service.controller.db;
 
 import com.codeaddi.scheduler_service.model.repository.Session;
 import com.codeaddi.scheduler_service.model.repository.SessionRepository;
+import jakarta.persistence.EntityNotFoundException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Slf4j
 @Service
 public class SessionsClient {
 
@@ -16,4 +19,27 @@ public class SessionsClient {
     public List<Session> getAllSessions(){
         return sessionRepository.findAll();
     }
+
+    public Session findById(Long id){
+        return sessionRepository.findById(id).orElseThrow(EntityNotFoundException::new);
+    }
+
+    public void replaceSession(Session newSession){
+        Session existingSession = findById(newSession.getId());
+
+        log.info("Deleting session: {}", existingSession.toString());
+
+        sessionRepository.deleteById(existingSession.getId());
+
+        log.info("Adding new session: {}", newSession);
+        sessionRepository.save(newSession);
+    }
+
+    public void addSession(Session newSession){
+        log.info("Adding new session: {}", newSession);
+
+        sessionRepository.save(newSession);
+    }
+
+
 }
