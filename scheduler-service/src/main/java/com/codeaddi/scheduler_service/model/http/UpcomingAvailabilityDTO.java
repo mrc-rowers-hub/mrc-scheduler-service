cb6cbe0adf944cdb4f86a190ed03a93f7ff6c743
AdelaideBaron <baronadelaide@hotmail.com>
2024-08-15 18:31:38 +0100

    dto for upcoming availability

---
+++ scheduler-service/src/main/java/com/codeaddi/scheduler_service/model/http/UpcomingAvailabilityDTO.java
@@ -0,0 +1,39 @@
+package com.codeaddi.scheduler_service.model.http;
+
+import com.codeaddi.scheduler_service.model.enums.RowerLevel;
+import com.codeaddi.scheduler_service.model.enums.SessionType;
+import com.codeaddi.scheduler_service.model.enums.Squad;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Value;
+import lombok.extern.jackson.Jacksonized;
+
+import java.time.LocalTime;
+import java.util.Date;
+
+@Builder
+@Value
+@AllArgsConstructor
+@Jacksonized
+public class UpcomingAvailabilityDTO {
+
+    @JsonProperty("upcoming_session_id")
+    private Long upcomingSessionId;
+
+    private Date date;
+
+    @JsonProperty("start_time")
+    private LocalTime startTime;
+
+    @JsonProperty("end_time")
+    private LocalTime endTime;
+
+    private Squad squad;
+
+    private RowerLevel level;
+
+    @JsonProperty("session_type")
+    private SessionType sessionType;
+
+}
