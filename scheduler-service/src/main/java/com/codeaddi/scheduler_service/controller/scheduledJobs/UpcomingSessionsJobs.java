a0c908f01c3a8816bfdcba09e1bc8c60f589ad72
AdelaideBaron <baronadelaide@hotmail.com>
2024-12-08 17:05:37 +0000

    updating sessions upon startup

--- scheduler-service/src/main/java/com/codeaddi/scheduler_service/controller/scheduledJobs/UpcomingSessionsJobs.java
+++ scheduler-service/src/main/java/com/codeaddi/scheduler_service/controller/scheduledJobs/UpcomingSessionsJobs.java
@@ -1,28 +1,43 @@
 package com.codeaddi.scheduler_service.controller.scheduledJobs;
 
 import com.codeaddi.scheduler_service.controller.db.UpcomingSessionsService;
+import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
-
 import java.time.DayOfWeek;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 
 @Component
 public class UpcomingSessionsJobs {
 
   @Autowired UpcomingSessionsService upcomingSessionsService;
 
-  @Scheduled(cron = "0 0 23 * * SAT") // Every Saturday at 11 PM
-  public void addNewUpcomingSessions() {
+  @PostConstruct
+  public void init() { // performed upon startup to clear anyuthing
+    // if now is saturday 8:01pm - weds 7:59pm, then perform the saturday one
+    // otherwise, do the wednesday one
+    LocalDateTime now = LocalDateTime.now();
+    DayOfWeek dayOfWeek = now.getDayOfWeek();
+    LocalTime time = now.toLocalTime();
+
+    if ((dayOfWeek == DayOfWeek.WEDNESDAY && time.isAfter(LocalTime.of(20, 1))) ||
+            (dayOfWeek.ordinal() < DayOfWeek.WEDNESDAY.ordinal())) {
+      addNewUpcomingSessionsSaturday();
+    } else {
+      addNewUpcomingSessionsWednesday();
+    }
+  }
+
+  @Scheduled(cron = "0 0 20 * * SAT") // Every Saturday at 8pm
+  public void addNewUpcomingSessionsSaturday() {
     upcomingSessionsService.addNewWeekOfUpcomingSessions(DayOfWeek.SATURDAY);
   }
 
-//  on weds at 8pm, cutoff those for this weekend
-//  on sat at 8pm, cutoff those for the coming week
-//  to cutoff, move sessions from upcoming to past
-//  and move availablility from upcoming to past
-  public void wednesdayCutoff(){
-
+  @Scheduled(cron = "0 0 20 * * WED") // Every Weds at 8pm
+  public void addNewUpcomingSessionsWednesday() {
+    upcomingSessionsService.addNewWeekOfUpcomingSessions(DayOfWeek.WEDNESDAY);
   }
 
 }
